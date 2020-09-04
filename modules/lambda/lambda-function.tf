65a977279b1b054401a83d7c68cc13336e39fb42
Nupur Goyal <37748784+nupur100@users.noreply.github.com>
2020-09-05 03:26:22 +0530

    Update lambda-function.tf

--- modules/lambda/lambda-function.tf
+++ modules/lambda/lambda-function.tf
@@ -1,8 +1,19 @@
+data "archive_file" "dummy" {
+  type        = "zip"
+  output_path = "${path.module}lambda_function.zip"
+  source {
+    content = <<EOF
+import json
+def lambda_handler(event, context):
+    print('Hello from Lambda!');
+EOF
+    filename = "lambda_function.py"
+  }
+}
+
 resource "aws_lambda_function" "amibackup-function" {
    function_name = "${var.function_name}"   
-   s3_bucket = "${var.s3_bucket}"
-   s3_key    = "${var.s3_key}"
-
+   filename = "${data.archive_file.dummy.output_path}"
    handler = "${var.handler}"
    runtime = "${var.runtime}"
    role = "${var.lambda_role}" 
