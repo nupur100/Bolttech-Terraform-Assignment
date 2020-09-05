52f56dda899f9bb857100e515fdd327633ab9fea
Nupur Goyal <37748784+nupur100@users.noreply.github.com>
2020-09-05 14:10:39 +0530

    Update instances.tf

--- modules/ec2/instances.tf
+++ modules/ec2/instances.tf
@@ -1,9 +1,18 @@
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.ec2_key_name}"
+  public_key = "${tls_private_key.this.public_key_openssh}"
+}
+
 resource "aws_instance" "web-server" {
     count = "${var.ec2_count}"
     ami = "${var.ami_id}"
     instance_type = "${var.instance_type}"
     subnet_id = "${var.subnet_id}"
-	key_name = "${var.ec2_key_name}"
+	key_name = "${aws_key_pair.generated_key.key_name}"
     tags = {
     Name = "${var.ec2_name}"
   }
